0408d52c4a11dd3341373bf7d7d554f5092c9dfc
Chris <47773518+chirs-moon@users.noreply.github.com>
2025-05-12 22:21:51 +0200

    Create Basic_Range.py

---
+++ Basic_Range.py
@@ -0,0 +1,22 @@
+# Input array function
+
+def get_arr_input() -> list[float]:
+    while True:
+        user_input = input("Please input your numbers separated by spaces: ").split()
+        try:
+            numbers = [float(i) for i in user_input]
+            if not numbers:
+                print("No numbers entered. Please try again.")
+                continue
+            return sorted(numbers)
+        except ValueError:
+            print("Invalid input detected. Please enter only numbers.")
+        
+# Calculation function
+
+def range(numbers) -> float:
+    return max(numbers) - min(numbers)
+    
+# Execution
+
+print(f"Range = {range(get_arr_input())}")
